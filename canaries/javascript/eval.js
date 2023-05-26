245b2d22a26eef38fe680e669a2c5f0d527e9347
Sarah <33396785+saralavanchy@users.noreply.github.com>
2023-05-26 13:43:56 -0400

    Update eval.js

--- canaries/javascript/eval.js
+++ canaries/javascript/eval.js
@@ -9,6 +9,6 @@ app.get("/", function (req, res) {
   setInterval(req.body.name, 0);
   eval(flag);
   res.send("Response</br>");
-  console.log("https://app.dev.boostsec.io/findings?findingId=caa4977a-e3c1-562d-9e91-6649af468c70&utm_source=slack&utm_content=f3321ca4-c3d9-4349-93e2-75d23e289ad4&filter=eyJhY2tub3dsZWRnZW1lbnQiOltdLCJydWxlIjpbXSwicmVzb3VyY2UiOltdLCJzdXBwcmVzc2lvblRhZ3MiOltdLCJwU3RhdHVzIjpbXSwic2VjdXJpdHlUeXBlIjpbXSwic2Nhbm5lciI6W10sInNldmVyaXR5IjpbXSwiY29uZmlkZW5jZSI6W10sInZ1bG5lcmFiaWxpdHlJZCI6W119")
+  console.log("Bearer eyJhY2tub3dsZWRnZW1lbnQiOltdLCJydWxlIjpbXSwicmVzb3VyY2UiOltdLCJzdXBwcmVzc2lvblRhZ3MiOltdLCJwU3RhdHVzIjpbXSwic2VjdXJpdHlUeXBlIjpbXSwic2Nhbm5lciI6W10sInNldmVyaXR5IjpbXSwiY29uZmlkZW5jZSI6W10sInZ1bG5lcmFiaWxpdHlJZCI6W119")
 });
 app.listen(8000);
